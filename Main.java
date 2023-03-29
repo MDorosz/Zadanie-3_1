66b6c6aea24cb4ed96db5eded3c7fd69673b5f7f
MAteusz Dorosz <126687548+MDorosz@users.noreply.github.com>
2023-03-29 12:38:23 +0000

    program rysujący kwadrat

--- Main.java
+++ Main.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-        
+
      Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Podaj wysokosc choinki: ");
+    System.out.print("Podaj wielkosc kwadratu: ");
      int n = scanner.nextInt();
 
     System.out.println();
-    
-        for (int i = 1; i <= n; i++) { // wyświetl poziom i-ty
-           
-          for (int j = 1; j <= n - i; j++) {
-          
-            System.out.print(" "); // wypełnienie spacjami na lewo
-            
-          }
-          
-          for (int k = 1; k <= i * 2 - 1; k++) {
-          
-            System.out.print("*"); // wyświetlenie gwiazdek
-            
-          }
-          
-          System.out.println();
-        }
+
+    for(int i = 1; i <= n; i++){
+      for(int j =1; j <= n; j++){
+        System.out.print("*");
+      }
+      System.out.println();
     }
-}
+    System.out.println();
+
+    for(int i = 1; i <= n; i++){
+      for(int j =1; j <= n; j++){
+        if (i>1 && i<n && j>1 && j<n){
+          System.out.print(" ");
+        }
+        else System.out.print("*");
+        }
+       System.out.println();
+      }
+    
+       System.out.println();
+    }
+  }
